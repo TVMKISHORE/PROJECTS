983e52063340c3fda9bd8ca5cd7f2aceffb0f656
TVMKISHORE <kishore.thadiboyina@gmail.com>
2017-04-09 19:21:48 +0530

    Update

--- LoanPrediction/Explore.py
+++
@@ -1,9 +0,0 @@
-#Import libraries:
-import pandas as pd
-import numpy as np
-import matplotlib.pylab as plt
-test.hist(column='LoanAmount', by='Gender')
-test.hist(column='LoanAmount', by='Gender')
-test.hist(column='LoanAmount', by='Gender')
-test.hist(column='LoanAmount', by='Education')
-test.hist(column='LoanAmount', by='Credit_History')
